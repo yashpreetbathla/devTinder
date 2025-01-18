06bdb91de52fe944e5e151a2321af496a83c0353
yashpreetbathla <yashpreetbathla@gmail.com>
2025-01-18 11:52:35 +0530

    removing cors error installing new lib

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const connectDB = require("./config/database");
 
 const app = express();
 const port = 3000;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
