c72cde412c74e6c4fd7abf76b85590a6adf308e3
yashpreetbathla <yashpreetbathla@gmail.com>
2025-01-20 00:01:40 +0530

    pwd db for testing

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://<userName>:<pwd>@namastenode.5uffz.mongodb.net/devTinder"
+    "mongodb+srv://namastedev:Ed1xPYX0q9JBQfke@namastenode.5uffz.mongodb.net/devTinder"
   );
 };
 
